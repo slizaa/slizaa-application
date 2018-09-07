47e807545b4a68492dc05e9c427475dbb777e0a7
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-09-07 14:48:56 +0200

    clean up

--- org.slizaa.application.server/src/main/java/org/slizaa/server/SlizaaServer.java
+++ org.slizaa.application.server/src/main/java/org/slizaa/server/SlizaaServer.java
@@ -2,6 +2,10 @@ package org.slizaa.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * <p>
